047df1a6fab0369b18600dd6387925aa3300f5db
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2020-04-21 21:34:48 +0200

    övning 1 klar

--- Nose-02/Nose-02/Program.cs
+++ Nose-02/Nose-02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Nose_02
 {
@@ -6,7 +7,22 @@ namespace Nose_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ÖVNING 1
+            string[] TOYs = { "toy car", "toy robot", "toy doll", "toy plane", "toy hero" };//array med 5 olika leksaker.
+            int rng = 0;//talet börjar med 0 då arrayer alltid läser av första platsen som 0
+            
+            for (int i = 0; i < 5; i++)
+                {
+                
+                string TOY = TOYs[rng];//den TOY som kallas är talet som rng är.
+                Console.WriteLine(TOY);
+                Console.ReadLine();
+                rng++;//efter varje gång adderas +1 till inten och då läser stringen en ny plats på arrayen.
+                }
+
+
+            string[] classmates = { "Collin", "Theodore", "Sebastian", "Benjamin", "Simon" };
+            int[] numbers = { 3, 5, 7, 8, 9};
         }
     }
 }
